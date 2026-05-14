09bf38bcceeb781e86f7b44c3a2b519d03898cbb
evnec <e.v.nechaev@gmail.com>
2026-05-14 13:06:03 +0000

    add developer compose file

---
+++ dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Development mode with hot reload
+echo "🚀 Starting development environment..."
+docker-compose -f docker-compose.dev.yml up --build
---
+++ prod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Production mode
+echo "🚀 Starting production environment..."
+docker-compose up --build -d
